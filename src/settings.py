a4a817ea5216ddd2cab70672b0a73f94c2cd00ac
rshalem <shalem000@gmail.com>
2020-08-30 13:09:35 +0530

    Final commit

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '8dj1q+ki1dro%y9j4+5j%6+c&9n4r)m=hkz9w50pu--5u1z4)$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -111,7 +111,7 @@ USE_TZ = True
 
 
 # when collectstatic command run, all static will be stored in this folder which is created when cmd is called
-STATIC_ROOT = (BASE_DIR / 'staticfiles')
+STATIC_ROOT = (BASE_DIR / 'staticfiles'),
 
 # url for serving static files
 STATIC_URL = '/static/'
@@ -126,5 +126,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'inspireme.py@gmail.com'
+EMAIL_HOST_PASSWORD = 'Hello123@'
